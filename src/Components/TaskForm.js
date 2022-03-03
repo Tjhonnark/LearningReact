68bf836ca61d690d4b35f198f126da0979be427d
Tjhonnark <tjhonnar@gmail.com>
2022-03-02 19:14:28 -0500

    React vs1.3

--- src/Components/TaskForm.js
+++ src/Components/TaskForm.js
@@ -1,12 +1,36 @@
 import React,  { Component } from 'react';
 
 export default class TaskForm extends Component {
+    
+    onChange = e => {
+        this.setState({
+            title: e.target.value
+        })
+    }
+
+    onSubmit = e => {
+        console.log('submiting');
+        e.preventDefault();
+    }
+
     render() {
         return (
-            <form>
-                <input type="text" placeholder="Write a Task"/>
+            <form onSubmit={this.onSubmit}> 
+                <input 
+                type="text" 
+                placeholder="Write a Task" 
+                onChange={this.onChange} 
+                value={this.state.title}
+                />
                 <br/>
-                <textarea placeholder="Write descripcion"></textarea>
+                <textarea 
+                placeholder="Write descripcion" 
+                onChange={this.onChange}
+                value={this.state.description}
+                />
+                <button type="submit">
+                    Enviar perro
+                </button>
             </form>
         )
     }
